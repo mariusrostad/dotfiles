f20a1ce97c88d3f4f65445b0e36ae0ed9eb3b758
Marius Rostad <31369489+mariusrostad@users.noreply.github.com>
2025-08-22 12:52:00 +0200

    rose-pine config

--- nvim/.config/nvim/lua/custom/plugins/rosepine.lua
+++ nvim/.config/nvim/lua/custom/plugins/rosepine.lua
@@ -5,6 +5,12 @@ return {
     -- vim.cmd 'colorscheme rose-pine'
   end,
   config = function()
+    require('rose-pine').setup {
+      extend_background_behind_borders = true,
+      styles = {
+        transparency = true,
+      },
+    }
     vim.cmd 'colorscheme rose-pine'
   end,
 }
