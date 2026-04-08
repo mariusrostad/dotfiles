a9e1c4024fe67ca46d60e7ac831a70b502068391
Marius Rostad <marostad98@gmail.com>
2026-04-08 21:31:19 +0200

    ocaml setup

--- fish/config.fish
+++ fish/config.fish
@@ -34,3 +34,13 @@ else
 	abbr -a ll 'ls -l'
 	abbr -a lll 'ls -la'
 end
+
+## OCaml setup
+#
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '~/.opam/opam-init/init.fish' && source '~/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
