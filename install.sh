40d94dcf3c2fa4f97f67de28958a329bd951153e
Marius Rostad <marostad98@gmail.com>
2026-04-08 21:29:05 +0200

    cleanup install script

--- install.sh
+++ install.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
 
-# Install xCode cli tools
-if [[ "$(uname)" == "Darwin" ]]; then
-    echo "macOS deteted..."
-
-    if xcode-select -p &>/dev/null; then
-        echo "Xcode already installed"
-    else
-        echo "Installing commandline tools..."
-        xcode-select --install
-    fi
+if [ ! -f /opt/homebrew/bin/brew ]; then
+    echo "Installing Brew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Brew already installed :)"
 fi
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-echo "Installing Brew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew analytics off
 
-## Taps
-echo "Tapping Brew..."
-brew tap FelixKratz/formulae
-
 ### Must Have things
-brew install zsh-autosuggestions
-brew install zsh-syntax-highlighting
 brew install atuin
 brew install eza # Bedre ls
 brew install stow # Linking fra dotfiles til config plasseringer
@@ -33,29 +17,27 @@ brew install fzf # Fuzzy find
 brew install bat
 brew install fd
 brew install zoxide
-brew install lua
-brew install luajit
-brew install luarocks
-brew install prettier
 brew install make
 brew install ripgrep
-
-## Download go lang
-curl -fsSL "https://go.dev/dl/go$GO_VERSION.darwin-arm64.pkg" -o "/tmp/go$GO_VERSION.darwin-arm64.pkg"
-sudo installer -pkg "/tmp/${PKG_FILE}" -target /
 
 ### Terminal
 brew install git
 brew install lazygit
 brew install tmux
 brew install neovim
-brew install starship
 brew install tree-sitter
 brew install tree
-brew install borders
 
-### dev things
-brew install node
+### For historical reasons
+# brew install lua
+# brew install luajit
+# brew install luarocks
+# brew install prettier
+# brew install starship
+# brew install borders
+# brew install zsh-autosuggestions
+# brew install zsh-syntax-highlighting
+# brew install node
 
 ## Casks
 brew install --cask betterdisplay
@@ -63,9 +45,6 @@ brew install --cask linearmouse
 brew install --cask font-hack-nerd-font
 brew install --cask font-jetbrains-mono-nerd-font
 brew install --cask font-sf-pro
-
-## Rust
-# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 ## MacOS settings
 # echo "Changing macOS defaults..."
@@ -88,6 +67,7 @@ cd $HOME/dotfiles || exit
 # Stow dotfiles packages
 echo "Stowing dotfiles..."
 stow -t ~ starship ghostty tmux zsh nvim claude
+stow -t ~/.config/fish fish
 
 sudo cp ./n/n /usr/local/bin/n && mkdir $HOME/.n/
 
